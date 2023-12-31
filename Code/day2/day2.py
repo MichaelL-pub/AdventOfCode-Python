bf35c589f00b375fb323f0383262f8592a38453b
Michael Leszczynski <michaellesz19@gmail.com>
2023-12-30 20:21:57 -0600

    added day2 part 2

--- Code/day2/day2.py
+++ Code/day2/day2.py
@@ -60,6 +60,33 @@ Determine which games would have been possible if the bag had been loaded with o
 
 so i think we just need to loop through the games, get the max number of each color in any turn and return any 
     games where the max number of cubes in each category than the numbers in the prompt
+    
+--- Part Two ---
+
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+    In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+    Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+    Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+    Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+    Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+-- end part 2 --
+
+We are already getting the minimum numbers for each color of cube, so we just need to return that rather than a true or false
 """
 
 import time
@@ -86,11 +113,16 @@ def isPossibleGame(game, redNum, greenNum, blueNum):
                 maxGreen = int(color.replace(" green",""))
             elif color.__contains__("blue") and int(color.replace(" blue","")) > maxBlue:
                 maxBlue = int(color.replace(" blue",""))
+                
+    gamePower = maxRed * maxGreen * maxBlue
+    print(str(gamePower))
+    
+    return gamePower
        
-    if maxRed <= redNum and maxGreen <= greenNum and maxBlue <= blueNum:
-        return True
-    else:
-        return False
+    # if maxRed <= redNum and maxGreen <= greenNum and maxBlue <= blueNum:
+    #     return True
+    # else:
+    #     return False
 
 start_time = time.time()
 
@@ -107,13 +139,12 @@ with open(directory +"day2.txt", "r") as file:
     line = file.readline()
     while line:
         
-        if isPossibleGame(line, redNum, greenNum, blueNum):
-            total += int(GameNum)
+        total += isPossibleGame(line, redNum, greenNum, blueNum)
         
         GameNum += 1
         line = file.readline()
         
-print("\n\nSum of possible game ids: ", total)
+print("\n\nSum of game powers: ", total)
         
 end_time = time.time()
 print("Execution time:", end_time - start_time, "seconds")
